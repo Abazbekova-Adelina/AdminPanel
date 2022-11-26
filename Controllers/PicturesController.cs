81bd781c811837112c2c4835d47f9107ec3755f2
bermet <116433177+tartapie@users.noreply.github.com>
2022-11-26 17:12:22 +0600

    Pictures Controller
    Имплеменнтировала интерйефс

--- Controllers/PicturesController.cs
+++ Controllers/PicturesController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminPanel.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AdminPanel.Controllers
 {
-    public class PicturesController : Controller
+    public class PicturesController : Controller, IPicturesCRUDable
     {
+        public Task<IActionResult> Create(string NewsName, string newsDescription, IFormFileCollection uploadedFiles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public Task<IActionResult> SetMainPictire(int IsMain)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IActionResult> IPicturesCRUDable.Index()
+        {
+            throw new NotImplementedException();
         }
     }
 }
